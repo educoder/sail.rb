36b97ef385e72e5273a9df6567e29ed5a3686e95
Matt Zukowski <matt@zukowski.ca>
2011-06-07 22:36:17 -0400

    changed Rollcall base configuration method

--- lib/sail/rollcall/base.rb
+++ lib/sail/rollcall/base.rb
@@ -2,12 +2,6 @@ require 'active_resource'
 
 module Rollcall
   class Base < ActiveResource::Base
-    XMPP_DOMAIN = "proto.encorelab.org"
-    
-    if SAIL_ENV == :development
-      self.site = "http://localhost:3000"
-    else
-      self.site = "http://rollcall.proto.encorelab.org"
-    end
+    self.site = ROLLCALL_URL
   end
 end
